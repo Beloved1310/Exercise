12f34509b6ace068f8d1160e4c57e844af545bc6
Beloved1310 <adeoluwafisayomi@gmail.com>
2021-09-20 16:44:53 +0100

    docs: initial commit

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ app.post("/api/users/:_id/exercises", (req,res) => {
     // ok
     const newExercise = new Exercise ({ _id, description, duration, date})
     newExercise.save((err, data)=>{
-      res.json({ _id, username, description,duration, date})
+      res.json({ _id, username, date, duration, description})
     })
   })
 })
